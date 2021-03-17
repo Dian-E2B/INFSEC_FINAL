73787a4b437c507944b83912591a4ffb2ecc99c0
NICEGOOD <ggranraniego@gmail.com>
2021-03-18 03:37:00 +0800

    Add validtion in add to cart function from cashier page

--- app/controllers/admin/cashier/create.php
+++ app/controllers/admin/cashier/create.php
@@ -5,10 +5,15 @@ include_once ('../../../config/functions.php');
 
   if (isset($_POST['operation'])) {
 
+    $row = findRow("SELECT * FROM products WHERE id = :id", $_POST['id']);
+
     if (empty($_POST['quantity'])) {
       echo 'This field is required.';
     } else if(!(is_numeric($_POST['quantity']) && $_POST['quantity'] > 0 && $_POST['quantity'] == round($_POST['quantity'], 0))) {
       echo 'Please enter a valid number.';
+    
+    } else if ($_POST['quantity'] >= $row['QuantityInStock']) {
+      echo 'Please enter a value less than or equal to ' . $row['QuantityInStock'];
     
     } else {
 
@@ -24,13 +29,13 @@ include_once ('../../../config/functions.php');
       $row = findRow("SELECT * FROM cart WHERE product_id = :id AND cart_code = -1", $_POST['id']);
 
       if (empty($row)) {
-        $query = "INSERT INTO cart (product_id, quantity, cart_code) VALUES (:product_id, :quantity, :cart_code)";
-        echo (!empty(insert($query, $data))) ? 'add to cart' : 'hehe';
-      }
+          $query = "INSERT INTO cart (product_id, quantity, cart_code) VALUES (:product_id, :quantity, :cart_code)";
+          echo (!empty(insert($query, $data))) ? 'add to cart' : 'hehe';
+      
+      } else if (!empty($row)) {
 
-      if (!empty($row)) {
-        $query = "UPDATE cart SET quantity = (quantity + :quantity) WHERE product_id = :product_id AND cart_code = :cart_code";
-        echo (!empty(update($query, $data))) ? 'add to cart' : 'hehe';
+        $query = "UPDATE cart SET quantity = (quantity + :quantity) WHERE cart_id = :cart_id";
+        echo (!empty(update($query, ['cart_id' => $row['cart_id']]))) ? 'add to cart' : 'hehe';
       }
 
       $data = [
